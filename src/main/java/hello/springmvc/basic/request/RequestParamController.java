bbe7083078a604e1a4faf45e5d4c6d79bda55725
Taexee <hee66265@naver.com>
2023-11-06 00:22:39 +0900

    HTTP 요청 파라미터@RequestParam 여러 방법

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -21,5 +24,59 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
     }
+
+    @ResponseBody //restController처럼 메시지바디에 그대로 문자열 반환, Controller는 리턴이 String이면 뷰를 찾는다
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName
+            , @RequestParam("age") int memberAge) {
+
+        log.info("memberName={}, memberAge={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username
+            , @RequestParam int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(@RequestParam(required = true) String username
+            , @RequestParam(required = false) Integer age) {
+        // true -> 꼭 있어야함 없으면 상태코드 400, false -> 없어도 됨 대신 int는 null이 불가 Integer 타입으로 변경
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(@RequestParam(required = true, defaultValue = "guest") String username
+            , @RequestParam(required = false, defaultValue = "-1") int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
 
 }
